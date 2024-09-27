f94d2ef12fb8e8df2d71467bdb77495241e99661
My Name <tinaliangge@hotmail.com>
2024-09-27 01:36:42 -0400

    add title and nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,88 @@
+// import ArticleItemList from "@/components/ArticleListItem"
+// import { getCategorisedArticles } from "@/lib/articles"
+
+// const HomePage = () => {
+//   const articles = getCategorisedArticles()
+
+//   console.log(articles)
+//   return (
+//     <section className="mx-auto w-11/12 md:w-1/2 mt-20 flex flex-col gap-16 mb-20">
+//       <header className="font-cormorantGaramond font-light text-6xl text-neutral-900 text-center mb-10">
+//         <h1>minimal blog</h1>
+//       </header>
+
+//       {/* Grid layout for articles */}
+//       <section className="grid grid-cols-1 gap-10">
+//         {articles !== null &&
+//           Object.keys(articles).map((article) => (
+//             <div
+//               className="border-b border-neutral-300 pb-5 hover:shadow-lg transition-shadow duration-300"
+//               key={article}
+//             >
+//               <ArticleItemList
+//                 category={article}
+//                 articles={articles[article]}
+//               />
+//             </div>
+//           ))}
+//       </section>
+//     </section>
+//   )
+// }
+
+// export default HomePage
+"use server";
+
 import ArticleItemList from "@/components/ArticleListItem"
-import { getCategorisedArticles } from "@/lib/articles"
+import { sql } from "@vercel/postgres";
+import NavBar from "../components/NavBar";
 
-const HomePage = () => {
-  const articles = getCategorisedArticles()
+export default async function HomePage({
+  params
+} : {
+  params: { category: string }
+}): Promise<JSX.Element> {
+  // Fetch articles based on the provided category
+  const { rows } = await sql`
+    SELECT * FROM articles
+  `;
 
-  console.log(articles)
   return (
-    <section className="mx-auto w-11/12 md:w-1/2 mt-20 flex flex-col gap-16 mb-20">
-      <header className="font-cormorantGaramond font-light text-6xl text-neutral-900 text-center mb-10">
-        <h1>minimal blog</h1>
-      </header>
+    <div>
+      <NavBar />
+      
+      <section className="pt-20">
+        {/* <header className="font-cormorantGaramond font-light text-6xl text-neutral-900 text-center mb-10">
+          <h1>minimal blog</h1>
+        </header> */}
 
-      {/* Grid layout for articles */}
-      <section className="grid grid-cols-1 gap-10">
-        {articles !== null &&
-          Object.keys(articles).map((article) => (
-            <div
-              className="border-b border-neutral-300 pb-5 hover:shadow-lg transition-shadow duration-300"
-              key={article}
-            >
-              <ArticleItemList
-                category={article}
-                articles={articles[article]}
-              />
-            </div>
-          ))}
+        <section className="grid grid-cols-1 gap-10 font-cormorantGaramond font-light text-3xl text-neutral-900">
+          {rows.length > 0 ? (
+            rows.map((row) => (
+              <div
+                className="border-b border-neutral-300 pb-5 hover:shadow-lg transition-shadow duration-300"
+                key={row.id}
+              >
+                  {row.title}
+                  {/* articles={articles[article]} */}
+                  
+                
+              </div>
+
+
+                // <h2>{row.title}</h2>
+                // <p>Category: {row.category}</p>
+                // <p>{new Date(row.date).toLocaleDateString()}</p>
+                // <img src={row.picture_url} alt={`${row.title} image`} />
+              // </div>
+            ))
+          ) : (
+            <p>No articles found for category: {params.category}</p>
+          )}
+        </section>
       </section>
-    </section>
-  )
+    </div>
+  );
 }
 
-export default HomePage
+{/* <ArticleItemList></ArticleItemList> */}
---
+++ components/NavBar.tsx
@@ -0,0 +1,168 @@
+"use client"
+
+import Link from "next/link";
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from '../public/logo.svg'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai';
+
+const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const handleNav = () : void => {
+        setMenuOpen(!menuOpen)
+    }
+
+  const links = [
+    {
+      id: 1,
+      link: "Tech",
+    },
+    {
+      id: 2,
+      link: "Reviews",
+    },
+    {
+      id: 3,
+      link: "Culture",
+    },
+    {
+      id: 4,
+      link: "AI",
+    },
+    {
+      id: 5,
+      link: "About Us",
+    },
+  ];
+
+  return (
+    <nav className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav">
+        <div className="flex-grow flex justify-center items-center">
+            <h1 className="text-5xl font-signature">
+                <Image src={Logo} alt="Logo" width="130" height="75" className="cursor-pointer"/>
+                {/* <a
+                className="link-underline link-underline-black"
+                href=""
+                target="_blank"
+                rel="noreferrer"
+                >
+                Logo
+                </a> */}
+            </h1>
+            
+        </div>
+
+        {/* Menu Icon */}
+        {/* <div onClick={handleNav} className="cursor-pointer pl-24">
+            {menuOpen ? (
+            <AiOutlineClose size={25} />
+            ) : (
+            <AiOutlineMenu size={25} />
+            )}
+        </div> */}
+        {/* <div onClick={handleNav} className="cursor-pointer pl-24">
+            <AiOutlineMenu size={25} >
+            </AiOutlineMenu>
+        </div>
+
+        
+
+        <div
+            className={
+            menuOpen
+                ? "fixed left-0 top-0 w-[65%] h-screen bg-[#000000] p-10 ease-in duration-500"
+                : "fixed left-[-100%] top-0 w-full h-screen p-10 ease-in duration-500"
+            }
+         >
+        </div> */}
+
+
+        {/* Hamburger and X icons */}
+        <div className="z-20 flex items-center">
+            <button className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:text-white focus:outline-none
+                                focus:ring-2 focus:ring-inset focus:ring-white" onClick={handleNav}> 
+                {
+                    menuOpen ? 
+                     (<svg
+                        className="h-6 w-6"
+                        xmlns="http://w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      > 
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                     </svg>)
+                     : 
+                     (<svg
+                        className="h-6 w-6"
+                        xmlns="http://w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      > 
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M4 6h16M4 12h16m-7 6h7"
+                      />
+                     </svg>)
+                }
+            </button>
+
+
+        </div>
+          
+        {/* Side Menu Panel */}
+        <div className={
+            menuOpen 
+            ? "fixed right-0 top-0 w-[25%] h-screen bg-[rgba(0,0,0,0.5)] p-10 ease-in duration-500 z-10" 
+            : "fixed right-[-100%] top-0 w-full h-screen p-10 ease-in duration-500"
+        }>
+            <ul className="flex flex-col space-y-4 text-white">
+                {links.map(({ id, link }) => (
+                <li
+                    key={id}
+                    className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-300 hover:scale-105 hover:text-white duration-200 link-underline"
+                >
+                    <Link href={link}>{link}</Link>
+                </li>
+                ))}
+            </ul>
+
+        </div>
+
+      {/* 
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              <Link onClick={() => setNav(!nav)} href={link}>
+                {link}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )} */}
+    </nav>
+  );
+};
+
+export default NavBar;
